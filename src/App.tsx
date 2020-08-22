19f47e0c25a943010062bd72c385718f3067af98
Don <donadley@gmail.com>
2020-08-21 21:05:57 -0500

    Copied NowWhat to Body
    Removed steps from body
    Copied weather into Metrics folder

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/Header';
 import Wrapper from './components/Wrapper';
-import NowWhat from './components/NowWhat';
+import Body from './components/Body';
 
 const store = createStore();
 const theme = createMuiTheme({
@@ -30,7 +30,7 @@ const App = () => (
     <Provider store={store}>
       <Wrapper>
         <Header />
-        <NowWhat />
+        <Body />
         <ToastContainer />
       </Wrapper>
     </Provider>
@@ -38,3 +38,9 @@ const App = () => (
 );
 
 export default App;
+
+
+// Add ability to select Metrics
+// Display the current metric data
+// Chart historical metric data
+// Submit Your App
---
+++ src/components/Body.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardHeader from './CardHeader';
+import Typography from '@material-ui/core/Typography';
+import CardContent from '@material-ui/core/CardContent';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { makeStyles } from '@material-ui/core/styles';
+import Avatar from './Avatar';
+
+const useStyles = makeStyles({
+  card: {
+    margin: '5% 25%',
+  },
+});
+
+export default () => {
+  const classes = useStyles();
+  return (
+    <Card className={classes.card}>
+      <CardHeader title="OK, donadley, you're all setup. Now What?" />
+      <CardContent>
+          
+      </CardContent>
+    </Card>
+  );
+};
